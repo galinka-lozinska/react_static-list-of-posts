971a944a5ea55e133788821ea496eee03d50ae7b
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-09-09 23:29:29 +0300

    Updates

---
+++ precache-manifest.119ba05b3730b65404bd79eb6a90ae6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af73565b3f4fe4fa6e410cf9a191a8e4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c1005150711fc409053",
+    "url": "./static/css/main.23267dd1.chunk.css"
+  },
+  {
+    "revision": "faf938ba8c46e764bc25",
+    "url": "./static/js/2.ab020241.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ab020241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c1005150711fc409053",
+    "url": "./static/js/main.fcf3f623.chunk.js"
+  },
+  {
+    "revision": "4d809184eca59fc21746",
+    "url": "./static/js/runtime-main.74611449.js"
+  }
+]);
